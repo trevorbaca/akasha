12d6dd273f5de7b3acad1bd40d0d0afe7e3902e0
Trevor Bača <trevorbaca@gmail.com>
2015-12-03 15:27:16 -0500

    Time signatures.

---
+++ akasha/materials/time_signatures_a/definition.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from abjad import *
+import baca
+
+
+numerators = [[3, 3, 4, 5], [4, 6, 6]]
+numerator_groups = baca.tools.helianthate(numerators, -1, 1)
+assert len(numerator_groups) == 24
+lengths = [len(_) for _ in numerator_groups]
+numerators = sequencetools.flatten_sequence(numerator_groups)
+time_signatures_a = [TimeSignature((_, 4)) for _ in numerators]
+time_signature_groups = sequencetools.partition_sequence_by_counts(
+    time_signatures_a,
+    lengths,
+    )
+time_signatures_a = time_signature_groups
