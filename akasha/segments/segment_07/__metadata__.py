bdca9d724ca9d0c28cd044aee1805cf89e252ef4
Trevor Bača <trevorbaca@gmail.com>
2015-12-28 20:52:49 -0600

    Time.

--- akasha/segments/segment_07/__metadata__.py
+++ akasha/segments/segment_07/__metadata__.py
@@ -26,10 +26,10 @@ metadata = datastructuretools.TypedOrderedDict(
                     ]
                 ),
             ),
-        ('end_tempo', None),
-        ('end_time_signature', '4/4'),
+        ('end_tempo', 55),
+        ('end_time_signature', '1/3'),
         ('first_bar_number', 137),
-        ('measure_count', 6),
+        ('measure_count', 48),
         ('name', 'F'),
         ('segment_count', 14),
         ('segment_number', 7),
