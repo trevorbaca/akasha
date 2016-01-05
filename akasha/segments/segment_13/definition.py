3baa79f9a1c8583625f07e93151f3646414e36ee
Trevor Bača <trevorbaca@gmail.com>
2016-01-04 18:22:00 -0600

    Time.

--- akasha/segments/segment_13/definition.py
+++ akasha/segments/segment_13/definition.py
@@ -10,22 +10,53 @@ from akasha.materials.__abbreviations__ import *
 ############################## SEGMENT-MAKER ##################################
 ###############################################################################
 
-time_signatures = akasha.materials.time_signatures_a[21:21+2]
+time_signature_start = 21
+time_signatures = akasha.materials.time_signatures_a
+time_signatures = sequencetools.rotate_sequence(
+    time_signatures,
+    time_signature_start,
+    )
 time_signatures = sequencetools.flatten_sequence(time_signatures)
-print(len(time_signatures))
-assert len(time_signatures) == 6
+
+stage_specifier = [
+    6,
+    1, TimeSignature((3, 3)),
+    2, 2, 2, TimeSignature((1, 3)), # stages 4-7
+    2, 2, 2, TimeSignature((1, 3)), # stages 8-11
+    2, 2, 4, TimeSignature((3, 3)), # stages 12-15
+    ]
+preprocessor = baca.tools.TimeSignaturePreprocessor(
+    stage_specifier=stage_specifier,
+    time_signatures=time_signatures,
+    )
+time_signature_groups = preprocessor()
+measures_per_stage = [len(_) for _ in time_signature_groups]
+time_signatures = sequencetools.flatten_sequence(time_signature_groups)
+
+tempo_map = (
+    (1, akasha.materials.tempi[55]),
+    (4, akasha.materials.tempi[89]),
+    )
+fermata_entries = preprocessor.make_fermata_entries()
+tempo_map = tempo_map + fermata_entries
+
+spacing_map = (
+    (1, Duration(1, 24)),
+    )
 
 segment_maker = baca.tools.SegmentMaker(
-    measures_per_stage=(
-        6,
-        ),
+    measures_per_stage=measures_per_stage,
     score_package=akasha,
-    show_stage_annotations=False,
-    tempo_map=(
-        ),
+    show_stage_annotations=True,
+    spacing_map=spacing_map,
+    tempo_map=tempo_map,
     time_signatures=time_signatures,
     )
 
-assert segment_maker.measure_count == 6
-assert segment_maker.stage_count == 1
+assert segment_maker.measure_count == 31, repr(segment_maker.measure_count)
+assert segment_maker.stage_count == 15, repr(segment_maker.stage_count)
 assert segment_maker.validate_time_signatures()
+
+###############################################################################
+################################ MUSIC-MAKERS #################################
+###############################################################################
