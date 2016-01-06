876caed10cd939a77484710c42701e0c09496c1a
Trevor Bača <trevorbaca@gmail.com>
2016-01-06 15:04:27 -0600

    Instrument names.

--- akasha/materials/instruments/definition.py
+++ akasha/materials/instruments/definition.py
@@ -1,68 +1,58 @@
 # -*- coding: utf-8 -*-
+import baca
 import collections
-from abjad import *
+from abjad.tools import instrumenttools
 
 
-long_space = 16
-short_space = 10
-
-def make_markup(string, space, column=True):
-    string_parts = string.split()
-    if len(string_parts) == 1:
-        markup = Markup(string_parts[0]).hcenter_in(space)
-    elif column:
-        markups = [Markup(_) for _ in string_parts]
-        markup = Markup.center_column(markups, direction=None)
-        markup = markup.hcenter_in(space)
-    else:
-        markups = [Markup(_) for _ in string_parts]
-        markup = Markup.line(*markups)
-        markup = markup.hcenter_in(space)
-    return markup
-
-def make_long_markup(string, column=False):
-    return make_markup(string, long_space, column=column)
-
-def make_short_markup(string, column=False):
-    return make_markup(string, short_space, column=column)
+agent = baca.tools.MarkupAgent
 
 instruments = collections.OrderedDict([
     (
         'violin 1', 
         instrumenttools.Violin(
             instrument_name='violin 1',
-            instrument_name_markup=make_long_markup('Violin 1', column=False),
+            instrument_name_markup=agent.make_instrument_name_markup(
+                'Violin 1',
+                column=False,
+                ),
             short_instrument_name='vn. 1',
-            short_instrument_name_markup=make_short_markup(
+            short_instrument_name_markup=\
+                agent.make_short_instrument_name_markup(
                 'Vn. 1',
                 column=False,
                 ),
-            )
+            ),
         ),
     (
         'violin 2', 
         instrumenttools.Violin(
             instrument_name='violin 2',
-            instrument_name_markup=make_long_markup('Violin 2', column=False),
+            instrument_name_markup=agent.make_instrument_name_markup(
+                'Violin 2',
+                column=False,
+                ),
             short_instrument_name='vn. 2',
-            short_instrument_name_markup=make_short_markup(
+            short_instrument_name_markup=\
+                agent.make_short_instrument_name_markup(
                 'Vn. 2',
                 column=False,
                 ),
-            )
+            ),
         ),
     (
         'viola', 
         instrumenttools.Viola(
-            instrument_name_markup=make_long_markup('Viola'),
-            short_instrument_name_markup=make_short_markup('Va.'),
-            )
+            instrument_name_markup=agent.make_instrument_name_markup('Viola'),
+            short_instrument_name_markup=\
+                agent.make_short_instrument_name_markup('Va.'),
+            ),
         ),
     (
         'cello', 
         instrumenttools.Cello(
-            instrument_name_markup=make_long_markup('Cello'),
-            short_instrument_name_markup=make_short_markup('Vc.'),
-            )
+            instrument_name_markup=agent.make_instrument_name_markup('Cello'),
+            short_instrument_name_markup=\
+                agent.make_short_instrument_name_markup('Vc.'),
+            ),
         ),
     ])
