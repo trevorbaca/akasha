796ab2edb7940c18af15702bda485768643aea1e
Trevor Bača <trevorbaca@gmail.com>
2016-01-12 22:28:32 -0600

    Color.

--- akasha/segments/segment_08/definition.py
+++ akasha/segments/segment_08/definition.py
@@ -10,9 +10,12 @@ from akasha.materials.__abbreviations__ import *
 ###############################################################################
 
 stage_specifier = baca.tools.StageSpecifier([
-    4, 3, 3,
+    4, # 1
+    3, # 2
+    3, # 3
     3, Fermata(), # 4-5
-    2, 1,
+    2, # 6
+    1, # 7
     ])
 
 tempo_map = baca.tools.TempoMap([
@@ -32,9 +35,9 @@ spacing_specifier = baca.tools.SpacingSpecifier(
     )
 
 segment_maker = baca.tools.SegmentMaker(
+    #label_stage_numbers=True,
     measures_per_stage=measures_per_stage,
     score_package=akasha,
-    label_stage_numbers=True,
     spacing_specifier=spacing_specifier,
     tempo_map=tempo_map,
     time_signatures=time_signatures,
@@ -48,154 +51,154 @@ segment_maker.validate_measures_per_stage()
 ################################# SPECIFIERS ##################################
 ###############################################################################
 
+### stage 1 ###
+
 segment_maker.append_specifiers(
     (vn_1, stages(1)),
-    baca.tools.RhythmSpecifier(
-        division_expression=baca.rhythm.fuse_compound_quarter_divisions([2, 2, 1, 2, 1])
-            .partition_by_ratio_of_lengths(Ratio((2, 1)))
-            [0]
-            ,
-        rhythm_maker=rhythmmakertools.EvenDivisionRhythmMaker(
-            denominators=[16],
-            extra_counts_per_division=[6, 4, 6, 3],
-            tie_specifier=rhythmmakertools.TieSpecifier(
-                tie_across_divisions=True,
-                ),
-            ),
+    akasha.tools.make_dense_getato_rhythm_specifier(
+        [2, 2, 1, 2, 1],
+        [6, 4, 6, 3],
         ),
     )
 
 segment_maker.append_specifiers(
-    (vn_2, stages(1)),
-    baca.tools.RhythmSpecifier(
-        rewrite_meter=True,
-        rhythm_maker=rhythmmakertools.NoteRhythmMaker(
-            tie_specifier=rhythmmakertools.TieSpecifier(
-                tie_across_divisions=True,
-                use_messiaen_style_ties=True,
-                ),
-            ),
-        ),
-    )
-
-segment_maker.append_specifiers(
-    (va, stages(1)),
-    baca.tools.RhythmSpecifier(
-        rewrite_meter=True,
-        rhythm_maker=rhythmmakertools.NoteRhythmMaker(
-            tie_specifier=rhythmmakertools.TieSpecifier(
-                tie_across_divisions=True,
-                use_messiaen_style_ties=True,
-                ),
-            ),
-        ),
-    )
-
-segment_maker.append_specifiers(
-    (vc, stages(1)),
-    baca.tools.RhythmSpecifier(
-        rewrite_meter=True,
-        rhythm_maker=rhythmmakertools.NoteRhythmMaker(
-            tie_specifier=rhythmmakertools.TieSpecifier(
-                tie_across_divisions=True,
-                use_messiaen_style_ties=True,
-                ),
-            ),
-        ),
+    ([vn_2, va, vc], stages(1)),
+    baca.rhythm.make_messiaen_note_rhythm_specifier(),
     )
 
 ### stage 2 ###
 
 segment_maker.append_specifiers(
-    (vn_1, stages(2)),
-    baca.tools.RhythmSpecifier(
-        rewrite_meter=True,
-        rhythm_maker=rhythmmakertools.NoteRhythmMaker(
-            tie_specifier=rhythmmakertools.TieSpecifier(
-                tie_across_divisions=True,
-                use_messiaen_style_ties=True,
-                ),
-            ),
-        ),
-    )
-
-segment_maker.copy_specifier(
-    (vn_2, 1),
-    (vn_2, stages(2)),
-    )
-
-segment_maker.copy_specifier(
-    (va, 1),
-    (va, stages(2)),
-    )
-
-segment_maker.copy_specifier(
-    (vc, 1),
-    (vc, stages(2)),
+    (tutti, stages(2)),
+    baca.rhythm.make_messiaen_note_rhythm_specifier(),
     )
 
 ### stage 3 ###
 
-segment_maker.copy_specifier(
-    (vn_1, 2),
-    (vn_1, stages(3)),
+
+segment_maker.append_specifiers(
+    (tutti, stages(3)),
+    baca.rhythm.make_messiaen_note_rhythm_specifier(),
     )
 
-segment_maker.copy_specifier(
-    (vn_2, 1),
-    (vn_2, stages(3)),
-    )
+### stage 4 + fermata ###
 
-segment_maker.copy_specifier(
-    (va, 1),
-    (va, stages(3)),
-    )
-
-segment_maker.copy_specifier(
-    (vc, 1),
-    (vc, stages(3)),
-    )
-
-### stages 4-5 ###
-
-segment_maker.copy_specifier(
-    (vn_1, 2),
-    (vn_1, stages(4)),
-    )
-
-segment_maker.copy_specifier(
-    (vn_2, 1),
-    (vn_2, stages(4)),
-    )
-
-segment_maker.copy_specifier(
-    (va, 1),
-    (va, stages(4)),
-    )
-
-segment_maker.copy_specifier(
-    (vc, 1),
-    (vc, stages(4)),
+segment_maker.append_specifiers(
+    (tutti, stages(4)),
+    baca.rhythm.make_messiaen_note_rhythm_specifier(),
     )
 
 ### stages 6-7 ###
 
-segment_maker.copy_specifier(
-    (vn_1, 2),
-    (vn_1, stages(6)),
+segment_maker.append_specifiers(
+    ([vn_1, vn_2, vc], stages(6)),
+    baca.rhythm.make_messiaen_note_rhythm_specifier(),
     )
 
-segment_maker.copy_specifier(
-    (vn_2, 1),
-    (vn_2, stages(6)),
-    )
-
-segment_maker.copy_specifier(
-    (va, 1),
+segment_maker.append_specifiers(
     (va, stages(6, 7)),
+    baca.rhythm.make_messiaen_tied_note_rhythm_specifier(),
     )
 
-segment_maker.copy_specifier(
-    (vc, 1),
-    (vc, stages(6)),
+###############################################################################
+#################################### COLOR ####################################
+###############################################################################
+
+### stage 1 ###
+
+segment_maker.append_specifiers(
+    (vn_1, stages(1)),
+    [
+        akasha.tools.make_getato_pitch_specifier(31, [2]),
+        Hairpin('pp > niente'),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (vn_1, stages(2, 6)),
+    [
+        baca.pitch.infinite_pitches([17, 19, 17, 15, 18, 16], [1]),
+        baca.spanners.glissandi(),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (vn_2, stages(1, 6)),
+    [
+        baca.pitch.infinite_pitches([6, 3, 5, 3, 1, 4], [1]),
+        baca.spanners.glissandi(),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (va, stages(1, 5)),
+    [
+        baca.pitch.infinite_pitches([3, 5, 2, 4, 2, 0], [1]),
+        baca.spanners.glissandi(),
+        Clef('alto'),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (va, stages(6)),
+    [
+        baca.dynamics.make_effort_dynamic('mf'),
+        baca.markup.OB(),
+        baca.spanners.one_line_staff(),
+        baca.pitch.pitches('C4'),
+        ],
+    )
+
+
+segment_maker.append_specifiers(
+    (vc, stages(1, 6)),
+    [
+        baca.pitch.infinite_pitches([-23, -21, -19, -22, -20, -22], [-1]),
+        baca.spanners.glissandi(),
+        Clef('bass'),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    ([vn_2, va, vc], stages(1)),
+    [
+        baca.spanners.make_transition(
+            baca.markup.tasto_scratch_moltiss(),
+            baca.markup.poco_scratch(),
+            ),
+        Hairpin('ff > f'),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (tutti, stages(2)),
+    [
+        baca.spanners.make_transition(
+            baca.markup.trans(),
+            baca.markup.tasto(),
+            ),
+        Hairpin('f > mf'),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (tutti, stages(3)),
+    [
+        baca.spanners.make_transition(
+            baca.markup.trans(),
+            baca.markup.FB_flaut(),
+            ),
+        Hairpin('mf > p'),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (tutti, stages(4)),
+    [
+        baca.spanners.make_transition(
+            baca.markup.trans(),
+            baca.markup.XFB_flaut(),
+            ),
+        Hairpin('p > pp'),
+        ],
     )
