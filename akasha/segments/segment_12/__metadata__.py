e564037b5a8d04731dfb06e2784db95e0500aac5
Trevor Bača <trevorbaca@gmail.com>
2016-01-14 10:31:40 -0600

    Color.

--- akasha/segments/segment_12/__metadata__.py
+++ akasha/segments/segment_12/__metadata__.py
@@ -26,7 +26,7 @@ metadata = datastructuretools.TypedOrderedDict(
                     ]
                 ),
             ),
-        ('end_tempo', 44),
+        ('end_tempo', 38),
         ('end_time_signature', '1/4'),
         ('first_bar_number', 265),
         ('measure_count', 68),
