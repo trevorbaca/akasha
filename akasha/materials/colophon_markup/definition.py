f27502d01050e2b981ae574d29cf6c3cc4dce117
Trevor Bača <trevorbaca@gmail.com>
2016-01-14 15:23:41 -0600

    Color.

--- akasha/materials/colophon_markup/definition.py
+++ akasha/materials/colophon_markup/definition.py
@@ -2,7 +2,7 @@
 from abjad import *
 
 
-place_markup = Markup('Cambridge, MA; Dallas; Madison, WI', direction=Up)
-date_markup = Markup('Oct. 2015 - Dec. 2015', direction=Up)
+place_markup = Markup('Cambridge, MA; Dallas, TX; Madison, WI', direction=Up)
+date_markup = Markup('Oct. - Dec. 2015', direction=Up)
 colophon_markup = Markup.right_column([place_markup, date_markup])
 colophon_markup = colophon_markup.italic()
