33698b0fc13280a8370f9b6211ecafc12292bed0
Trevor Bača <trevor.baca@gmail.com>
2016-04-23 14:32:23 -0500

    Ported markup forward.

--- akasha/materials/instruments/definition.py
+++ akasha/materials/instruments/definition.py
@@ -4,20 +4,18 @@ import collections
 from abjad.tools import instrumenttools
 
 
-agent = baca.tools.MarkupAgent
-
 instruments = collections.OrderedDict([
     (
         'violin 1', 
         instrumenttools.Violin(
             instrument_name='violin 1',
-            instrument_name_markup=agent.make_instrument_name_markup(
+            instrument_name_markup=baca.markup.make_instrument_name_markup(
                 'Violin 1',
                 column=False,
                 ),
             short_instrument_name='vn. 1',
             short_instrument_name_markup=\
-                agent.make_short_instrument_name_markup(
+                baca.markup.make_short_instrument_name_markup(
                 'Vn. 1',
                 column=False,
                 ),
@@ -27,13 +25,13 @@ instruments = collections.OrderedDict([
         'violin 2', 
         instrumenttools.Violin(
             instrument_name='violin 2',
-            instrument_name_markup=agent.make_instrument_name_markup(
+            instrument_name_markup=baca.markup.make_instrument_name_markup(
                 'Violin 2',
                 column=False,
                 ),
             short_instrument_name='vn. 2',
             short_instrument_name_markup=\
-                agent.make_short_instrument_name_markup(
+                baca.markup.make_short_instrument_name_markup(
                 'Vn. 2',
                 column=False,
                 ),
@@ -42,17 +40,19 @@ instruments = collections.OrderedDict([
     (
         'viola', 
         instrumenttools.Viola(
-            instrument_name_markup=agent.make_instrument_name_markup('Viola'),
+            instrument_name_markup=baca.markup.make_instrument_name_markup(
+                'Viola'),
             short_instrument_name_markup=\
-                agent.make_short_instrument_name_markup('Va.'),
+                baca.markup.make_short_instrument_name_markup('Va.'),
             ),
         ),
     (
         'cello', 
         instrumenttools.Cello(
-            instrument_name_markup=agent.make_instrument_name_markup('Cello'),
+            instrument_name_markup=baca.markup.make_instrument_name_markup(
+                'Cello'),
             short_instrument_name_markup=\
-                agent.make_short_instrument_name_markup('Vc.'),
+                baca.markup.make_short_instrument_name_markup('Vc.'),
             ),
         ),
     ])
