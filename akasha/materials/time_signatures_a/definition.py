328b0fb3d5ae13832abb2b64acbaded5a081d849
Trevor Bača <trevor.baca@gmail.com>
2016-12-30 11:15:14 -0600

    Ported transform calls.

--- akasha/materials/time_signatures_a/definition.py
+++ akasha/materials/time_signatures_a/definition.py
@@ -4,7 +4,7 @@ import baca
 
 
 numerators = [[3, 3, 4, 5], [4, 6, 6]]
-numerator_groups = baca.tools.helianthate(numerators, -1, 1)
+numerator_groups = baca.transforms.helianthate(numerators, -1, 1)
 assert len(numerator_groups) == 24
 lengths = [len(_) for _ in numerator_groups]
 numerators = abjad.sequencetools.flatten_sequence(numerator_groups)
--- akasha/materials/time_signatures_b/definition.py
+++ akasha/materials/time_signatures_b/definition.py
@@ -4,7 +4,7 @@ import baca
 
 
 numerators = [[3, 6, 7, 7], [4, 8, 9, 9], [3, 4]]
-numerator_groups = baca.tools.helianthate(numerators, -1, 1)
+numerator_groups = baca.transforms.helianthate(numerators, -1, 1)
 assert len(numerator_groups) == 36
 lengths = [len(_) for _ in numerator_groups]
 numerators = abjad.sequencetools.flatten_sequence(numerator_groups)
