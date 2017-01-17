959ead21e19c8179bbe3a7c690dc0f8613745b6c
Trevor Bača <trevor.baca@gmail.com>
2017-01-16 19:46:00 -0600

    Updated pitch references.

--- akasha/materials/cello_solo_pitches/definition.py
+++ akasha/materials/cello_solo_pitches/definition.py
@@ -2,7 +2,7 @@
 import abjad
 
 
-cello_solo_pitches = abjad.pitchtools.PitchSegment(
+cello_solo_pitches = abjad.PitchSegment(
     'E3 F3 F+3 F#3 C3 B2 B-2 Bb2 Ab2 A2 B2 C#3 C3 E3 E-3 Eb3 Db3 C3 D3 F#3',
     item_class=abjad.NamedPitch,
     )
