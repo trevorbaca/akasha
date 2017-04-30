d877462bb64d9f3d25a7371df79256227779099b
Trevor Bača <trevor.baca@gmail.com>
2017-04-29 19:51:05 -0500

    Cleaned up package initializer.

--- akasha/__init__.py
+++ akasha/__init__.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import distutils.version
+import platform
+if not (
+    distutils.version.LooseVersion('3.5') <
+    distutils.version.LooseVersion(platform.python_version())
+    ):
+    raise ImportError('Requires Python 3.5.')
+del distutils
+del platform
 from akasha import tools
 from akasha import materials
 from akasha import segments
