b795bf0b2bd7845c4ca23ab3410589e37b55b428
Trevor Bača <trevor.baca@gmail.com>
2017-07-06 09:28:49 -0500

    Changed Tempo to MetronomeMark.

--- akasha/materials/tempi/definition.py
+++ akasha/materials/tempi/definition.py
@@ -5,22 +5,22 @@ import abjad
 tempi = abjad.TypedOrderedDict([
     (
         38,
-        abjad.Tempo(abjad.Duration(1, 4), 38),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 38),
         ),
     (
         44,
-        abjad.Tempo(abjad.Duration(1, 4), 44),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 44),
         ),
     (
         55,
-        abjad.Tempo(abjad.Duration(1, 4), 55),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 55),
         ),
     (
         89,
-        abjad.Tempo(abjad.Duration(1, 4), 89),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 89),
         ),
     (
         126,
-        abjad.Tempo(abjad.Duration(1, 4), 126),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 126),
         ),
     ])
