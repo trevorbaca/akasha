c09fea35853e46c935239e00c43dcaff67076e97
Trevor Bača <trevor.baca@gmail.com>
2017-08-03 09:23:49 -0500

    Integrated namespace updates.

--- akasha/materials/reference_meters/definition.py
+++ akasha/materials/reference_meters/definition.py
@@ -5,5 +5,5 @@ import abjad
 string = 6 * ['(1/4 (1/8 1/8))']
 string = ' '.join(string)
 string = '(6/4 ({}))'.format(string)
-meter = abjad.metertools.Meter(string, preferred_boundary_depth=1)
+meter = abjad.Meter(string, preferred_boundary_depth=1)
 reference_meters = [meter]
