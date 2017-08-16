bc00afc2a7dcd293fc6f81e94a90e69a363a3f6b
Trevor Bača <trevor.baca@gmail.com>
2017-08-16 11:32:02 -0500

    Cleaned up metadat.

--- akasha/__metadata__.py
+++ akasha/__metadata__.py
@@ -4,10 +4,8 @@ import abjad
 metadata = abjad.TypedOrderedDict(
     [
         ('catalog_number', 'AWN-013'),
-        ('composer', 'Bača'),
+        ('composer', 'Trevor Bača'),
         ('forces_tagline', 'for string quartet'),
-        ('paper_size', '17 x 11 in'),
-        ('price', '\\$60 / \\euro 54'),
         ('title', 'Akasha'),
         ('year', 2015),
         ]
