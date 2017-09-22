14ca3038bcd2dcbb6cab57d74a523fda11baf141
Trevor Bača <trevor.baca@gmail.com>
2017-09-22 16:16:38 -0500

    Renamed segments.

---
+++ akasha/segments/J/__metadata__.py
@@ -0,0 +1,36 @@
+import abjad
+
+
+metadata = abjad.TypedOrderedDict(
+    [
+        (
+            'end_clefs_by_staff',
+            abjad.TypedOrderedDict(
+                [
+                    ('Cello Music Staff', 'bass'),
+                    ('Viola Music Staff', None),
+                    ('Violin One Music Staff', None),
+                    ('Violin Two Music Staff', None),
+                    ]
+                ),
+            ),
+        (
+            'end_instruments_by_context',
+            abjad.TypedOrderedDict(
+                [
+                    ('Cello Music Staff', 'cello'),
+                    ('Viola Music Staff', 'viola'),
+                    ('Violin One Music Staff', 'violin 1'),
+                    ('Violin Two Music Staff', 'violin 2'),
+                    ]
+                ),
+            ),
+        ('end_tempo', 44),
+        ('end_time_signature', '1/4'),
+        ('first_bar_number', 261),
+        ('measure_count', 4),
+        ('name', 'J'),
+        ('segment_count', 15),
+        ('segment_number', 11),
+        ]
+    )
