b46fb072114e592e9ea2e295ce25607fb17c7813
Trevor Bača <trevor.baca@gmail.com>
2017-10-07 10:40:19 -0500

    Integrated f-strings.

--- akasha/materials/reference_meters/definition.py
+++ akasha/materials/reference_meters/definition.py
@@ -3,6 +3,6 @@ import abjad
 
 string = 6 * ['(1/4 (1/8 1/8))']
 string = ' '.join(string)
-string = '(6/4 ({}))'.format(string)
+string = f'(6/4 ({string}))'
 meter = abjad.Meter(string, preferred_boundary_depth=1)
 reference_meters = [meter]
