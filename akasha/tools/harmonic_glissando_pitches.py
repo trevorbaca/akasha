bce586637c6ca0e7aced23d446fd30d0156cce76
Trevor Bača <trevor.baca@gmail.com>
2017-11-06 09:26:27 -0600

    Integrating API changes.

--- akasha/tools/harmonic_glissando_pitches.py
+++ akasha/tools/harmonic_glissando_pitches.py
@@ -5,15 +5,17 @@ import baca
 
 def harmonic_glissando_pitches(
     start_pitch,
-    direction=Up,
+    direction=abjad.Up,
     rotation=None,
     ):
+    r'''Makes harmonic glissando pitches.
+    '''
     start_pitch = abjad.NumberedPitch(start_pitch)
     start_pitch = start_pitch.number
     pitch_numbers = akasha.materials.getato_intervals
     pitch_numbers = [3 * _ for _ in pitch_numbers]
-    if direction is Down:
+    if direction == abjad.Down:
         pitch_numbers = [-_ for _ in pitch_numbers]
     pitch_numbers = [_ + start_pitch for _ in pitch_numbers]
     pitch_numbers = abjad.sequence(pitch_numbers).rotate(n=rotation)
-    return baca.ScorePitchCommand(source=pitch_numbers)
+    return baca.pitches(pitch_numbers)
