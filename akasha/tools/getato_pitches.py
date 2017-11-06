1de000260bcf97ee444a1dd3c456b31ef80282af
Trevor Bača <trevor.baca@gmail.com>
2017-11-06 09:26:27 -0600

    Integrating API changes.

--- akasha/tools/getato_pitches.py
+++ akasha/tools/getato_pitches.py
@@ -6,15 +6,14 @@ import baca
 def getato_pitches(
     start_pitch,
     repetition_intervals=[0],
-    direction=Up,
+    direction=abjad.Up,
     ):
+    r'''Makes getato pitches.
+    '''
     start_pitch = abjad.NumberedPitch(start_pitch)
     start_pitch = start_pitch.number
     pitch_numbers = akasha.materials.getato_intervals
-    if direction is Down:
+    if direction == abjad.Down:
         pitch_numbers = [-_ for _ in pitch_numbers]
     pitch_numbers = [_ + start_pitch for _ in pitch_numbers]
-    return baca.ScorePitchCommand(
-        repetition_intervals=repetition_intervals,
-        source=pitch_numbers,
-        )
+    return baca.infinite_pitches(pitch_numbers, repetition_intervals)
