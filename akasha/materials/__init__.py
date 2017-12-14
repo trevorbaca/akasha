b03bfdb2ede68216d61e73c204cd4630214a9c26
Trevor Bača <trevor.baca@gmail.com>
2017-12-14 11:44:42 -0600

    Cleaned up initializers.

--- akasha/materials/__init__.py
+++ akasha/materials/__init__.py
@@ -1,7 +1,7 @@
-import abjad
-
-
-abjad.ImportManager.import_material_packages(
-    __path__[0],
-    globals(),
-    )
+from .colophon_markup.definition import colophon_markup
+from .getato_intervals.definition import getato_intervals
+from .instruments.definition import instruments
+from .metronome_marks.definition import metronome_marks
+from .reference_meters.definition import reference_meters
+from .time_signatures_a.definition import time_signatures_a
+from .time_signatures_b.definition import time_signatures_b
