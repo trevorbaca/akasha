e8ce8e9193d8911ed08f3a1ae27ceaa107481b23
Trevor Bača <trevor.baca@gmail.com>
2017-12-16 11:23:17 -0600

    Cleaning up metadata.

--- akasha/segments/B/__metadata__.py
+++ akasha/segments/B/__metadata__.py
@@ -49,6 +49,7 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
+        ('end_margin_markup', None),
         ('end_metronome_mark', '55'),
         (
             'end_staff_lines',
--- akasha/segments/I/__metadata__.py
+++ akasha/segments/I/__metadata__.py
@@ -49,6 +49,7 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
+        ('end_margin_markup', None),
         ('end_metronome_mark', '44'),
         (
             'end_staff_lines',
