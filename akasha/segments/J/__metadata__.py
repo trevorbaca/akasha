7b7324325e6627d7e7f4ac4c9cf97fd1a2a80e52
Trevor Bača <trevor.baca@gmail.com>
2017-12-18 15:38:31 -0600

    Cleaning up persistent indicator handling.

--- akasha/segments/J/__metadata__.py
+++ akasha/segments/J/__metadata__.py
@@ -3,9 +3,8 @@ import abjad
 
 metadata = abjad.TypedOrderedDict(
     [
-        ('duration', "0'21''"),
         (
-            'end_clefs',
+            'abjad.Clef',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -28,7 +27,7 @@ metadata = abjad.TypedOrderedDict(
                 ),
             ),
         (
-            'end_dynamics',
+            'abjad.Dynamic',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -51,7 +50,7 @@ metadata = abjad.TypedOrderedDict(
                 ),
             ),
         (
-            'end_instruments',
+            'abjad.Instrument',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -73,9 +72,8 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
-        ('end_margin_markup', None),
         (
-            'end_metronome_marks',
+            'abjad.MetronomeMark',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -85,6 +83,19 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
+        (
+            'abjad.TimeSignature',
+            abjad.TypedOrderedDict(
+                [
+                    (
+                        'Score',
+                        ('1/4', 'GlobalSkips'),
+                        ),
+                    ]
+                ),
+            ),
+        ('baca.MarginMarkup', None),
+        ('duration', "0'21''"),
         (
             'end_staff_lines',
             abjad.TypedOrderedDict(
@@ -100,17 +111,6 @@ metadata = abjad.TypedOrderedDict(
                     (
                         'ViolinTwoMusicStaff',
                         (1, 'ViolinTwoMusicVoice'),
-                        ),
-                    ]
-                ),
-            ),
-        (
-            'end_time_signatures',
-            abjad.TypedOrderedDict(
-                [
-                    (
-                        'Score',
-                        ('1/4', 'GlobalSkips'),
                         ),
                     ]
                 ),
