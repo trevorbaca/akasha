893f117d2ac20e76d5852484baba8b5ff71ab52c
Trevor Bača <trevor.baca@gmail.com>
2018-05-31 13:40:16 -0500

    Cleaned up Travis configfile.

--- setup.py
+++ setup.py
@@ -2,14 +2,29 @@
 import setuptools
 
 
+install_requires = [
+    'abjad',
+    'mypy',
+    'roman',
+    ]
+
+keywords = [
+    'abjad',
+    'music composition',
+    'music notation',
+    'formalized score control',
+    'lilypond',
+    ]
+keywords = ', '.join(keywords)
+
 if __name__ == '__main__':
     setuptools.setup(
         author='Trevor Bača',
         author_email='trevor.baca@gmail.com',
-        install_requires=('abjad',),
-        name='akasha',
-        packages=('akasha',),
+        install_requires=install_requires,
+        keywords=keywords,
+        name='Akasha',
+        packages=['akasha'],
+        paltforms='Any',
         url='https://github.com/trevorbaca/akasha',
-        version='0.1',
-        zip_safe=False,
         )
