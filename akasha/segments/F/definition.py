083ebf7ce37151ad8ce995ebdfab866566e8c6b8
Trevor Bača <trevor.baca@gmail.com>
2018-06-05 11:13:51 -0500

    Integrated markup changes.

--- akasha/segments/F/definition.py
+++ akasha/segments/F/definition.py
@@ -465,7 +465,10 @@ maker(
 
 maker(
     ('GlobalSkips', (17, 18)),
-    baca.markups.boxed_repeat_count(6, baca.skip(0)),
+    baca.markup(
+        baca.markups.boxed_repeat_count(6),
+        selector=baca.skip(0),
+        ),
     baca.text_script_extra_offset((1.5, 6)),
     baca.volta(),
     )
