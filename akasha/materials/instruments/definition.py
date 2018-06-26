15e39491728ad616b33c27102a079fa1bc760581
Trevor Bača <trevor.baca@gmail.com>
2018-06-26 12:24:29 -0500

    Cleaned up instrument definitions.

--- akasha/materials/instruments/definition.py
+++ akasha/materials/instruments/definition.py
@@ -5,18 +5,18 @@ import baca
 instruments = abjad.OrderedDict([
     (
         'ViolinI',
-        abjad.Violin(hide=True, pitch_range='[G3, +inf]'),
+        abjad.Violin(pitch_range='[G3, +inf]'),
         ),
     (
         'ViolinII',
-        abjad.Violin(hide=True, pitch_range='[G3, +inf]'),
+        abjad.Violin(pitch_range='[G3, +inf]'),
         ),
     (
         'Viola',
-        abjad.Viola(hide=True, pitch_range='[A2, +inf]'),
+        abjad.Viola(pitch_range='[A2, +inf]'),
         ),
     (
         'Cello',
-        abjad.Cello(hide=True, pitch_range='[A1, +inf]'),
+        abjad.Cello(pitch_range='[A1, +inf]'),
         ),
     ])
