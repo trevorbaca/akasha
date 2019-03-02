54da7b5368e49eed495869a5913a72dae10733f7
Trevor Bača <trevor.baca@gmail.com>
2019-03-02 12:17:48 -0600

    Cleaned up typehints.

--- akasha/__init__.py
+++ akasha/__init__.py
@@ -9,5 +9,12 @@ del distutils
 del platform
 
 from akasha.tools import *
-from akasha.materials import *
+from akasha.materials.colophon_markup.definition import colophon_markup
+from akasha.materials.getato_intervals.definition import getato_intervals
+from akasha.materials.instruments.definition import instruments
+from akasha.materials.margin_markups.definition import margin_markups
+from akasha.materials.metronome_marks.definition import metronome_marks
+from akasha.materials.reference_meters.definition import reference_meters
+from akasha.materials.time_signature_series.definition import \
+    time_signature_series
 from akasha import segments
--- akasha/materials/reference_meters/definition.py
+++ akasha/materials/reference_meters/definition.py
@@ -1,8 +1,11 @@
 import abjad
 
 
-string = 6 * ['(1/4 (1/8 1/8))']
-string = ' '.join(string)
+strings = 6 * ['(1/4 (1/8 1/8))']
+string = ' '.join(strings)
 string = f'(6/4 ({string}))'
-meter = abjad.Meter(string, preferred_boundary_depth=1)
+meter = abjad.Meter(
+    string,
+    preferred_boundary_depth=1,
+    )
 reference_meters = [meter]
