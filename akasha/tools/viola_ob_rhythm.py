32a97bfd37233c53cea40cafee3036189d222432
Trevor Bača <trevor.baca@gmail.com>
2019-05-31 12:56:23 -0500

    Cleaned up division handling.

--- akasha/tools/viola_ob_rhythm.py
+++ akasha/tools/viola_ob_rhythm.py
@@ -3,13 +3,13 @@ import baca
 from abjadext import rmakers
 
 
-def viola_ob_rhythm(*, rotation=None) -> baca.RhythmCommand:
+def viola_ob_rhythm(*, rotation: int = None) -> baca.RhythmCommand:
     """
     Makes viola OB rhythm.
     """
-    durations = baca.sequence([(1, 4), (1, 4), (3, 8), (1, 4), (3, 8)])
-    durations = durations.rotate(n=rotation)
-    divisions = baca.divisions().fuse().split_each(durations, cyclic=True)
+    divisions = baca.sequence([(1, 4), (1, 4), (3, 8), (1, 4), (3, 8)])
+    divisions = divisions.rotate(n=rotation)
+    divisions = baca.divisions().fuse().split(divisions, cyclic=True)
     return baca.rhythm(
         divisions=divisions,
         rhythm_maker=rmakers.NoteRhythmMaker(
