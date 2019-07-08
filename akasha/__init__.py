9484a26acdbee2b7cdd9dc65d904802fa3502170
Trevor Bača <trevor.baca@gmail.com>
2019-07-08 18:03:17 -0400

    Cleaned up rhythm construction.

--- akasha/__init__.py
+++ akasha/__init__.py
@@ -15,7 +15,6 @@ from akasha.materials.getato_intervals.definition import getato_intervals
 from akasha.materials.instruments.definition import instruments
 from akasha.materials.margin_markups.definition import margin_markups
 from akasha.materials.metronome_marks.definition import metronome_marks
-from akasha.materials.reference_meters.definition import reference_meters
 from akasha.materials.time_signature_series.definition import (
     time_signature_series,
 )
--- akasha/materials/reference_meters/definition.py
+++
@@ -1,8 +0,0 @@
-import abjad
-
-
-strings = 6 * ["(1/4 (1/8 1/8))"]
-string = " ".join(strings)
-string = f"(6/4 ({string}))"
-meter = abjad.Meter(string, preferred_boundary_depth=1)
-reference_meters = [meter]
