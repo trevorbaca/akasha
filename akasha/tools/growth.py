eccb429ce41a255c72509775c148c588f5b73d95
Trevor Bača <trevor.baca@gmail.com>
2019-07-08 14:57:30 -0400

    Cleaned up rhythm construction.

--- akasha/tools/growth.py
+++ akasha/tools/growth.py
@@ -22,56 +22,55 @@ def growth(
     if special is True:
         assert index is None
 
-    #        ratio = abjad.Ratio(division_ratio)
-    #        divisions = baca.divisions().fuse().split([(1, 4)], cyclic=True)
-    #        divisions = divisions.flatten(depth=-1)
-    #        divisions = divisions.partition_by_ratio_of_lengths(ratio)
-    #        divisions = divisions.fuse(indices=[1, 3, 5])
-    #
-    #        accelerando_rhythm_maker = rmakers.AccelerandoRhythmMaker(
-    #            rmakers.TupletSpecifier(duration_bracket=True),
-    #            ###rmakers.SilenceMask(selector=baca.lts()[pattern]),
-    #            rmakers.SilenceMask(selector=baca.lts()),
-    #            rmakers.BeamSpecifier(
-    #                beam_rests=True,
-    #                selector=baca.tuplets(),
-    #                stemlet_length=0.75,
-    #                use_feather_beams=True,
-    #            ),
-    #            interpolation_specifiers=[
-    #                rmakers.InterpolationSpecifier(
-    #                    start_duration=(1, 2),
-    #                    stop_duration=(1, 8),
-    #                    written_duration=(1, 16),
-    #                )
-    #            ],
-    #            tag=tag,
-    #        )
-    #
-    #        talea_rhythm_maker = rmakers.TaleaRhythmMaker(
-    #            ###rmakers.SilenceMask(selector=baca.lts()[pattern]),
-    #            rmakers.BeamSpecifier(selector=baca.tuplets()),
-    #            rmakers.TupletSpecifier(extract_trivial=True),
-    #            # TODO: teach TieSpecifier to respect (1, 4):
-    #            #rmakers.TieSpecifier(repeat_ties=(1, 4)),
-    #            rmakers.TieSpecifier(repeat_ties=True),
-    #            extra_counts_per_division=extra_counts,
-    #            tag=tag,
-    #            talea=rmakers.Talea(counts=[9, 4, 8, 7], denominator=16),
-    #        )
-    #
-    #        return baca.rhythm(
-    #            divisions=divisions,
-    #            rhythm_maker=[
-    #                baca.DivisionAssignment(
-    #                    abjad.DurationInequality(">", (1, 4)),
-    #                    accelerando_rhythm_maker,
-    #                ),
-    #                baca.DivisionAssignment(
-    #                    abjad.index([0], 1), talea_rhythm_maker,
-    #                ),
-    #            ],
-    #        )
+        ratio = abjad.Ratio(division_ratio)
+        divisions = baca.divisions().fuse().split([(1, 4)], cyclic=True)
+        divisions = divisions.flatten(depth=-1)
+        divisions = divisions.partition_by_ratio_of_lengths(ratio)
+        divisions = divisions.fuse(indices=[1, 3, 5])
+
+        accelerando_rhythm_maker = rmakers.AccelerandoRhythmMaker(
+            rmakers.TupletSpecifier(duration_bracket=True),
+            rmakers.SilenceMask(selector=baca.lts()[pattern]),
+            rmakers.BeamSpecifier(
+                beam_rests=True,
+                selector=baca.tuplets(),
+                stemlet_length=0.75,
+                use_feather_beams=True,
+            ),
+            interpolation_specifiers=[
+                rmakers.InterpolationSpecifier(
+                    start_duration=(1, 2),
+                    stop_duration=(1, 8),
+                    written_duration=(1, 16),
+                )
+            ],
+            tag=tag,
+        )
+
+        talea_rhythm_maker = rmakers.TaleaRhythmMaker(
+            rmakers.SilenceMask(selector=baca.lts()[pattern]),
+            rmakers.BeamSpecifier(selector=baca.tuplets()),
+            rmakers.TupletSpecifier(extract_trivial=True),
+            # TODO: teach TieSpecifier to respect (1, 4):
+            #rmakers.TieSpecifier(repeat_ties=(1, 4)),
+            rmakers.TieSpecifier(repeat_ties=True),
+            extra_counts_per_division=extra_counts,
+            tag=tag,
+            talea=rmakers.Talea(counts=[9, 4, 8, 7], denominator=16),
+        )
+
+        return baca.rhythm(
+            divisions=divisions,
+            rhythm_maker=baca.DivisionAssignments(
+                baca.DivisionAssignment(
+                    abjad.DurationInequality(">", (1, 4)),
+                    accelerando_rhythm_maker,
+                ),
+                baca.DivisionAssignment(
+                    abjad.index([0], 1), talea_rhythm_maker,
+                ),
+            ),
+        )
 
     else:
 
