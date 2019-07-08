22ab04771600594afe1346c84cef23b8e85fe9a7
Trevor Bača <trevor.baca@gmail.com>
2019-07-08 18:03:17 -0400

    Cleaned up rhythm construction.

--- akasha/materials/__init__.py
+++ akasha/materials/__init__.py
@@ -3,5 +3,4 @@ from .getato_intervals.definition import getato_intervals
 from .instruments.definition import instruments
 from .margin_markups.definition import margin_markups
 from .metronome_marks.definition import metronome_marks
-from .reference_meters.definition import reference_meters
 from .time_signature_series.definition import time_signature_series
