ddad229b4418af6eb474236d150f083d989e4f8e
Trevor Bača <trevor.baca@gmail.com>
2019-07-16 00:02:29 -0400

    Cleaned up rhythm construction.

--- akasha/tools/accelerando_rhythm.py
+++ akasha/tools/accelerando_rhythm.py
@@ -35,7 +35,7 @@ def accelerando_rhythm(
                 ]
             ),
             *specifiers,
-            rmakers.set_duration_bracket(),
+            rmakers.duration_bracket(),
             rmakers.feather_beam(beam_rests=True, stemlet_length=0.75),
             divisions=divisions,
         ),
