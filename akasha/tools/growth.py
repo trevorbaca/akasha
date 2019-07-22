c9201bc920354fd805518307dd53b58fe1d954fe
Trevor Bača <trevor.baca@gmail.com>
2019-07-22 00:43:41 -0400

    Cleaned up rhythm construction.

--- akasha/tools/growth.py
+++ akasha/tools/growth.py
@@ -22,35 +22,32 @@ def growth(
     divisions = divisions.partition_by_ratio_of_lengths(ratio)
     divisions = divisions.fuse(indices=[1, 3, 5])
 
-    accelerando_rhythm_maker = rmakers.accelerando(
+    accelerando_command = rmakers.rhythm(
+        rmakers.accelerando(
+            interpolations=rmakers.interpolate((1, 2), (1, 8), (1, 16))
+        ),
         rmakers.force_rest(baca.lts().get(pattern)),
         rmakers.feather_beam(beam_rests=True, stemlet_length=0.75),
         rmakers.duration_bracket(),
-        interpolations=rmakers.interpolate((1, 2), (1, 8), (1, 16)),
     )
 
-    talea_rhythm_maker = rmakers.talea(
-        [9, 4, 8, 7],
-        16,
+    talea_command = rmakers.rhythm(
+        rmakers.talea([9, 4, 8, 7], 16, extra_counts=extra_counts),
         rmakers.force_rest(baca.lts().get(pattern)),
         rmakers.beam(),
         rmakers.rewrite_rest_filled(),
         rmakers.rewrite_sustained(),
         rmakers.extract_trivial(),
         rmakers.force_repeat_tie(threshold=(1, 4)),
-        extra_counts=extra_counts,
     )
 
     return baca.rhythm(
         rmakers.rhythm(
             rmakers.MakerAssignments(
                 rmakers.MakerAssignment(
-                    abjad.DurationInequality(">", (1, 4)),
-                    accelerando_rhythm_maker,
+                    abjad.DurationInequality(">", (1, 4)), accelerando_command
                 ),
-                rmakers.MakerAssignment(
-                    abjad.index([0], 1), talea_rhythm_maker
-                ),
+                rmakers.MakerAssignment(abjad.index([0], 1), talea_command),
             ),
             preprocessor=divisions,
         ),
