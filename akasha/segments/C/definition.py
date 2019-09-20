6752a0aff5fd276c5d096dee355802fdd0ef783c
Trevor Bača <trevor.baca@gmail.com>
2019-09-20 16:31:34 -0400

    Rerendered segments.

--- akasha/segments/C/definition.py
+++ akasha/segments/C/definition.py
@@ -40,6 +40,7 @@ maker = baca.SegmentMaker(
     activate=[
         abjad.const.LOCAL_MEASURE_NUMBER,
         ],
+    check_all_are_pitched=True,
     color_octaves=False,
     ignore_repeat_pitch_classes=True,
     segment_directory=abjad.Path(os.path.realpath(__file__)).parent,
