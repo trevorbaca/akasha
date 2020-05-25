b26816fbc321187e187d7360391a761a76e7fc77
Trevor Bača <trevor.baca@gmail.com>
2020-05-25 14:49:44 -0400

    Cleaned up typehints.

--- akasha/tools.py
+++ akasha/tools.py
@@ -68,8 +68,8 @@ groups = numerators.helianthate(-1, 1)
 assert len(groups) == 24
 lengths = [len(_) for _ in groups]
 numerators = baca.sequence(groups).flatten(depth=-1)
-time_signatures = [abjad.TimeSignature((_, 4)) for _ in numerators]
-groups = baca.sequence(time_signatures).partition_by_counts(lengths)
+_time_signatures = [abjad.TimeSignature((_, 4)) for _ in numerators]
+groups = baca.sequence(_time_signatures).partition_by_counts(lengths)
 time_signature_series["A"] = groups
 
 numerators = baca.sequence([[3, 6, 7, 7], [4, 8, 9, 9], [3, 4]])
@@ -77,8 +77,8 @@ groups = numerators.helianthate(-1, 1)
 assert len(groups) == 36
 lengths = [len(_) for _ in groups]
 numerators = baca.sequence(groups).flatten(depth=-1)
-time_signatures = [abjad.TimeSignature((_, 8)) for _ in numerators]
-groups = baca.sequence(time_signatures).partition_by_counts(lengths)
+_time_signatures = [abjad.TimeSignature((_, 8)) for _ in numerators]
+groups = baca.sequence(_time_signatures).partition_by_counts(lengths)
 time_signature_series["B"] = groups
 
 # rhtyhms
