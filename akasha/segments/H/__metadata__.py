2a1770eab8f30653247d15ca1cdae873c22eb9b9
Trevor Bača <trevor.baca@gmail.com>
2020-08-27 13:07:31 -0400

    Blacked code.

--- akasha/segments/H/__metadata__.py
+++ akasha/segments/H/__metadata__.py
@@ -4,24 +4,24 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
-            'bol_measure_numbers',
+            "bol_measure_numbers",
             [217],
-            ),
-        ('duration', "0'22''"),
+        ),
+        ("duration", "0'22''"),
         (
-            'fermata_measure_numbers',
+            "fermata_measure_numbers",
             [220, 223],
-            ),
-        ('final_measure_is_fermata', True),
-        ('final_measure_number', 223),
-        ('first_measure_number', 217),
-        ('segment_name', 'H'),
-        ('segment_number', 9),
-        ('start_clock_time', "11'06''"),
-        ('stop_clock_time', "11'28''"),
+        ),
+        ("final_measure_is_fermata", True),
+        ("final_measure_number", 223),
+        ("first_measure_number", 217),
+        ("segment_name", "H"),
+        ("segment_number", 9),
+        ("start_clock_time", "11'06''"),
+        ("stop_clock_time", "11'28''"),
         (
-            'time_signatures',
-            ['4/4', '5/4', '3/4', '1/4', '3/4', '4/4', '1/4'],
-            ),
-        ]
-    )
+            "time_signatures",
+            ["4/4", "5/4", "3/4", "1/4", "3/4", "4/4", "1/4"],
+        ),
+    ]
+)
--- akasha/segments/_/__metadata__.py
+++ akasha/segments/_/__metadata__.py
@@ -4,24 +4,24 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
-            'bol_measure_numbers',
+            "bol_measure_numbers",
             [1],
-            ),
-        ('duration', "0'15''"),
+        ),
+        ("duration", "0'15''"),
         (
-            'fermata_measure_numbers',
+            "fermata_measure_numbers",
             [3],
-            ),
-        ('final_measure_is_fermata', True),
-        ('final_measure_number', 3),
-        ('first_measure_number', 1),
-        ('segment_name', '_'),
-        ('segment_number', 1),
-        ('start_clock_time', "0'00''"),
-        ('stop_clock_time', "0'15''"),
+        ),
+        ("final_measure_is_fermata", True),
+        ("final_measure_number", 3),
+        ("first_measure_number", 1),
+        ("segment_name", "_"),
+        ("segment_number", 1),
+        ("start_clock_time", "0'00''"),
+        ("stop_clock_time", "0'15''"),
         (
-            'time_signatures',
-            ['3/8', '6/8', '1/4'],
-            ),
-        ]
-    )
+            "time_signatures",
+            ["3/8", "6/8", "1/4"],
+        ),
+    ]
+)
