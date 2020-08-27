8531e6e3d2ad736c6626b1116deaf92c02c80f8e
Trevor Bača <trevor.baca@gmail.com>
2020-08-27 13:07:31 -0400

    Blacked code.

--- akasha/segments/L/__metadata__.py
+++ akasha/segments/L/__metadata__.py
@@ -4,23 +4,23 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
-            'bol_measure_numbers',
+            "bol_measure_numbers",
             [334],
-            ),
-        ('duration', "0'37''"),
+        ),
+        ("duration", "0'37''"),
         (
-            'fermata_measure_numbers',
+            "fermata_measure_numbers",
             [335, 337],
-            ),
-        ('final_measure_number', 339),
-        ('first_measure_number', 334),
-        ('segment_name', 'L'),
-        ('segment_number', 13),
-        ('start_clock_time', "19'18''"),
-        ('stop_clock_time', "19'55''"),
+        ),
+        ("final_measure_number", 339),
+        ("first_measure_number", 334),
+        ("segment_name", "L"),
+        ("segment_number", 13),
+        ("start_clock_time", "19'18''"),
+        ("stop_clock_time", "19'55''"),
         (
-            'time_signatures',
-            ['4/4', '1/4', '6/4', '1/4', '6/4', '3/4'],
-            ),
-        ]
-    )
+            "time_signatures",
+            ["4/4", "1/4", "6/4", "1/4", "6/4", "3/4"],
+        ),
+    ]
+)
