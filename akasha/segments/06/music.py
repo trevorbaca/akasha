f2a25b0566898541660e24ba9e9ed17e8b2d7ecf
Trevor Bača <trevor.baca@gmail.com>
2021-07-28 17:37:37 -0400

    Cleaned up package initializer.

--- akasha/segments/06/music.py
+++ akasha/segments/06/music.py
@@ -3,7 +3,7 @@ import pathlib
 import baca
 from abjadext import rmakers
 
-import akasha
+from akasha import library as akasha
 
 #########################################################################################
 ########################################### 06 ##########################################
