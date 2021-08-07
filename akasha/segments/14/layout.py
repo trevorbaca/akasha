a2232c5eec06e3529c1cb6bcd42ceadcfd342ed4
Trevor Bača <trevor.baca@gmail.com>
2021-08-07 13:36:38 -0400

    Cleaned up layout files.

--- akasha/segments/14/layout.py
+++ akasha/segments/14/layout.py
@@ -2,8 +2,8 @@ import baca
 
 breaks = baca.breaks(
     baca.page(
-        baca.system(measure=340, y_offset=20, distances=(20, 20)),
-        baca.system(measure=354, y_offset=140, distances=(20, 20)),
+        baca.system(measure=1, y_offset=20, distances=(20, 20)),
+        baca.system(measure=15, y_offset=140, distances=(20, 20)),
     ),
 )
 
