df9441092265c52df8cb7b230ca0543d83abe372
Trevor Bača <trevor.baca@gmail.com>
2021-08-12 17:18:37 -0400

    Cleaned up layout files.

--- akasha/segments/08/layout.py
+++ akasha/segments/08/layout.py
@@ -1,11 +1,13 @@
 import baca
 
-breaks = baca.breaks(
-    baca.page(
-        baca.system(measure=1, y_offset=20, distances=(20, 20)),
-        baca.system(measure=8, y_offset=140, distances=(20, 20)),
-    ),
-)
-
 if __name__ == "__main__":
-    baca.build.make_layout_ly(breaks, fallback_duration=(1, 32))
+    spacing = baca.SpacingSpecifier(
+        breaks=baca.breaks(
+            baca.page(
+                baca.system(measure=1, y_offset=20, distances=(20, 20)),
+                baca.system(measure=8, y_offset=140, distances=(20, 20)),
+            ),
+        ),
+        fallback_duration=(1, 32),
+    )
+    baca.build.make_layout_ly(spacing)
--- akasha/segments/09/layout.py
+++ akasha/segments/09/layout.py
@@ -1,10 +1,12 @@
 import baca
 
-breaks = baca.breaks(
-    baca.page(
-        baca.system(measure=1, y_offset=20, distances=(20, 20)),
-    ),
-)
-
 if __name__ == "__main__":
-    baca.build.make_layout_ly(breaks, fallback_duration=(1, 24))
+    spacing = baca.SpacingSpecifier(
+        breaks=baca.breaks(
+            baca.page(
+                baca.system(measure=1, y_offset=20, distances=(20, 20)),
+            ),
+        ),
+        fallback_duration=(1, 24),
+    )
+    baca.build.make_layout_ly(spacing)
