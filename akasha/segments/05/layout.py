730da88ed0f4df54f6aa0144d426c87be3f06200
Trevor Bača <trevor.baca@gmail.com>
2021-08-23 12:11:23 -0400

    Cleaned up layout files.

--- akasha/segments/05/layout.py
+++ akasha/segments/05/layout.py
@@ -3,14 +3,14 @@ import baca
 spacing = baca.SpacingSpecifier(
     breaks=baca.breaks(
         baca.page(
+            1,
             baca.system(measure=1, y_offset=30, distances=(20, 20)),
             baca.system(measure=10, y_offset=155, distances=(20, 20)),
-            number=1,
         ),
         baca.page(
+            2,
             baca.system(measure=24, y_offset=30, distances=(20, 20)),
             baca.system(measure=38, y_offset=155, distances=(20, 20)),
-            number=2,
         ),
     ),
     fallback_duration=(1, 20),
