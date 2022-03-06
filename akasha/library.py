d0dcbff78c2be4ed15cc85c78bc6f9c121aedddf
Trevor Bača <trevor.baca@gmail.com>
2022-03-06 15:31:50 +0100

    Cleaned up collections.

--- akasha/library.py
+++ akasha/library.py
@@ -98,9 +98,8 @@ def cello_solo_pitches(transposition=None):
     """
     Makes cello solo pitches.
     """
-    pitches = abjad.PitchSegment(
+    pitches = abjad.NamedPitchSegment(
         "E3 F3 F+3 F#3 C3 B2 B-2 Bb2 Ab2 A2 B2 C#3 C3 E3 E-3 Eb3 Db3 C3 D3 F#3",
-        item_class=abjad.NamedPitch,
     )
     if transposition is not None:
         pitches = pitches.transpose(transposition)
