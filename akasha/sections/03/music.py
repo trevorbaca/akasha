65020469f9366448e9890de728b39a73c8c5c124
Trevor Bača <trevor.baca@gmail.com>
2022-03-13 11:50:44 +0100

    Cleaned up selection.

--- akasha/sections/03/music.py
+++ akasha/sections/03/music.py
@@ -155,7 +155,7 @@ commands(
     (["va", "vc"], 11),
     library.material(
         "D",
-        selector=lambda _: baca.select.rleaves(_),
+        selector=lambda _: baca.rleaves(_),
     ),
     baca.make_repeat_tied_notes(),
     baca.markup(r"\baca-tasto-markup"),
