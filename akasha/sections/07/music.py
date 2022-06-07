2cf85be8149133b3a56dfad0d3cc7db435345dc7
Trevor Bača <trevor.baca@gmail.com>
2022-06-07 17:44:03 +0200

    Cleaned up music.py files.

--- akasha/sections/07/music.py
+++ akasha/sections/07/music.py
@@ -55,6 +55,7 @@ commands = baca.CommandAccumulator(
 
 baca.interpret.set_up_score(
     score,
+    commands,
     commands.manifests(),
     commands.time_signatures,
     append_anchor_skip=True,
@@ -93,9 +94,10 @@ commands(
 
 commands(
     "Skips",
-    baca.open_volta(lambda _: baca.select.skip(_, 45 - 1)),
     baca.close_volta(lambda _: baca.select.skip(_, 47 - 1)),
 )
+
+baca.commands._open_volta(skips[45 - 1], commands.first_measure_number)
 
 commands(
     "Rests",
