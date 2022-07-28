ce9795062b73444950657e277afca79475cae2e0
Trevor Bača <trevor.baca@gmail.com>
2022-07-28 17:51:39 -0400

    Cleaned up music.py files.

--- akasha/library.py
+++ akasha/library.py
@@ -457,7 +457,6 @@ def material_annotation_spanner_function(argument, letter):
     baca.markup_function(
         abjad.select.leaf(argument, 0),
         rf"\akasha-material-{letter.lower()}",
-        allow_rests=True,
     )
     material_to_color = {
         "A": "0.984 0.945 0.492",
