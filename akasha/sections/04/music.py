0ecc317d2fa1cb71c84dccb3eb74ab63ca5539fc
Trevor Bača <trevor.baca@gmail.com>
2022-10-03 09:38:37 -0400

    Cleaned up metadata.

--- akasha/sections/04/music.py
+++ akasha/sections/04/music.py
@@ -345,7 +345,7 @@ def main():
         environment.previous_metadata["persistent_indicators"],
         environment.timing,
     )
-    metadata, persist = baca.section.postprocess_score(
+    metadata = baca.section.postprocess_score(
         score,
         measures(),
         **baca.section.section_defaults(),
@@ -371,7 +371,6 @@ def main():
     baca.build.persist_lilypond_file(
         lilypond_file,
         metadata,
-        persist,
         environment.timing,
         environment.arguments,
     )
