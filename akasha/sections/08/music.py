336862e844ae92dbeb37f34c510e6916f40c6a7f
Trevor Bača <trevor.baca@gmail.com>
2023-04-02 13:48:58 -0400

    Cleaned up music.py files.

--- akasha/sections/08/music.py
+++ akasha/sections/08/music.py
@@ -227,7 +227,7 @@ def make_score(first_measure_number, previous_persistent_indicators):
     for abbreviation in ["va"]:
         voice = voices(abbreviation)
         baca.section.append_anchor_note(voice)
-    baca.section.reapply(
+    baca.section.reapply_persistent_indicators(
         voices,
         previous_persistent_indicators,
         manifests=library.manifests,
