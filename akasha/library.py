895b89bd859a781520aee25584c3112811b0ed27
Trevor Bača <trevor.baca@gmail.com>
2023-04-15 09:44:40 -0400

    Cleaned up metronome marks.

--- akasha/library.py
+++ akasha/library.py
@@ -510,11 +510,11 @@ instruments = {
 
 
 metronome_marks = {
-    "38": abjad.MetronomeMark((1, 4), 38),
-    "44": abjad.MetronomeMark((1, 4), 44),
-    "55": abjad.MetronomeMark((1, 4), 55),
-    "89": abjad.MetronomeMark((1, 4), 89),
-    "126": abjad.MetronomeMark((1, 4), 126),
+    "38": abjad.MetronomeMark(abjad.Duration(1, 4), 38),
+    "44": abjad.MetronomeMark(abjad.Duration(1, 4), 44),
+    "55": abjad.MetronomeMark(abjad.Duration(1, 4), 55),
+    "89": abjad.MetronomeMark(abjad.Duration(1, 4), 89),
+    "126": abjad.MetronomeMark(abjad.Duration(1, 4), 126),
 }
 
 
