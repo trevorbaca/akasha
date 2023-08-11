ebc9cf5fea84b265beb4e7486d72fcc25c8f160e
Trevor Bača <trevor.baca@gmail.com>
2023-08-11 15:08:40 -0400

    Rerendered sections.

--- akasha/sections/01/music.py
+++ akasha/sections/01/music.py
@@ -103,7 +103,6 @@ def make_score():
         time_signatures(),
         append_anchor_skip=True,
         always_make_global_rests=True,
-        first_section=True,
         manifests=library.manifests,
     )
     GLOBALS(score["Skips"], score["Rests"])
@@ -134,6 +133,7 @@ def main():
         empty_fermata_measures=True,
         error_on_not_yet_pitched=True,
         fermata_extra_offset_y=4.5,
+        first_section=True,
         global_rests_in_topmost_staff=True,
     )
     baca.tags.deactivate(
