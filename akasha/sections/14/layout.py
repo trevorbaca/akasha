073e319b50339dfab9c8deb5a05509840886c1de
Trevor Bača <trevor.baca@gmail.com>
2023-09-08 13:15:43 -0400

    Cleaned up layout.py files.

--- akasha/sections/14/layout.py
+++ akasha/sections/14/layout.py
@@ -1,13 +1,17 @@
 import baca
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=20, distances=(20, 20)),
-        baca.system(measure=15, y_offset=140, distances=(20, 20)),
-    ),
-    spacing=(1, 12),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=20, distances=(20, 20)),
+            baca.system(measure=15, y_offset=140, distances=(20, 20)),
+        ),
+        spacing=(1, 12),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
--- akasha/sections/15/layout.py
+++ akasha/sections/15/layout.py
@@ -1,13 +1,17 @@
 import baca
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=20, distances=(20, 20)),
-        baca.system(measure=14, y_offset=140, distances=(20, 20)),
-    ),
-    spacing=(1, 22),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=20, distances=(20, 20)),
+            baca.system(measure=14, y_offset=140, distances=(20, 20)),
+        ),
+        spacing=(1, 22),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
