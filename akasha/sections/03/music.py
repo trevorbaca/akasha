00ee56724fd471ffe7f1d166a31dd246e82fafc7
Trevor Bača <trevor.baca@gmail.com>
2023-09-21 12:42:31 -0400

    Cleaned up music.py files.

--- akasha/sections/03/music.py
+++ akasha/sections/03/music.py
@@ -238,5 +238,16 @@ def main():
     )
 
 
+def make_layout():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=30, distances=(20, 20, 20, 20)),
+        ),
+        spacing=(1, 24),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()
--- akasha/sections/04/music.py
+++ akasha/sections/04/music.py
@@ -376,5 +376,17 @@ def main():
     )
 
 
+def make_layout():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=30, distances=(20, 20)),
+            baca.system(measure=15, y_offset=150, distances=(20, 20)),
+        ),
+        spacing=(1, 16),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()
