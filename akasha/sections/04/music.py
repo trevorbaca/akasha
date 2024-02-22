85b7aa65c8d62b171f7a7cdcd5a0d1c6312baa67
Trevor Bača <trevor.baca@gmail.com>
2024-02-22 08:55:13 -0500

    Cleaned up music.py files.

--- akasha/sections/04/music.py
+++ akasha/sections/04/music.py
@@ -233,11 +233,11 @@ def v2(m):
 def va(m):
     for n in [1, 3, 5, 7]:
         baca.pitches(m[n], "D#3 C+3", exact=True)
-        baca.glissando(m[n])
+        baca.basic_glissando(m[n])
         library.material_annotation_spanner(m[n], "D")
     with baca.scope(m[9, 12]) as o:
         baca.pitches(o, "Eb3 D3 C#3 B#2", exact=True)
-        baca.glissando(o)
+        baca.basic_glissando(o)
         baca.hairpin(
             o,
             "mp>pp",
@@ -259,7 +259,7 @@ def va(m):
             o,
             "mp>pp",
         )
-        baca.glissando(o)
+        baca.basic_glissando(o)
         baca.markup(o.pleaf(0), r"\baca-tasto-markup")
         library.material_annotation_spanner(o, "D")
 
