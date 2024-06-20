7db56dd56b97073bc5069db63738881832918e8e
Trevor Bača <trevor.baca@gmail.com>
2024-06-20 08:07:06 +0200

    Cleaned up layout files.

--- akasha/sections/07/music.py
+++ akasha/sections/07/music.py
@@ -572,7 +572,7 @@ def make_layout(environment):
             baca.layout.Override(45, (1, 48)),
         ),
     )
-    return baca.build.write_layout_ily(
+    return baca.section.make_layout_score(
         breaks,
         environment.metadata["time_signatures"],
         fermata_measure_numbers=environment.metadata.get("fermata_measure_numbers", []),
