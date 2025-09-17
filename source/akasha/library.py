23c9a25cb8992f025f0bdc662eda61efa48eeed5
Trevor Bača <trevor.baca@gmail.com>
2025-09-17 16:27:04 -0400

    Clean up accelerando interpolation-handling

--- source/akasha/library.py
+++ source/akasha/library.py
@@ -126,8 +126,10 @@ def make_accelerando_rhythm(
         durations = [sum(_) for _ in durations]
     tuplets = rmakers.accelerando(
         abjad.duration.durations(durations),
-        abjad.duration.durations([(1, 2), (1, 8), (1, 16)]),
-        abjad.duration.durations([(1, 8), (1, 2), (1, 16)]),
+        baca.rhythm.interpolations(
+            [(1, 2), (1, 8), (1, 16)],
+            [(1, 8), (1, 2), (1, 16)],
+        ),
         tag=tag,
     )
     voice = rmakers.wrap_in_time_signature_staff(tuplets, time_signatures)
@@ -251,7 +253,7 @@ def make_growth_rhythm(
             duration = sum(duration_list)
             tuplets = rmakers.accelerando(
                 [duration],
-                abjad.duration.durations([(1, 2), (1, 8), (1, 16)]),
+                baca.rhythm.interpolations([(1, 2), (1, 8), (1, 16)]),
                 tag=tag,
             )
             time_signatures = [abjad.TimeSignature(duration.pair())]
@@ -342,8 +344,10 @@ def make_ritardando_rhythm(time_signatures, *, force_rest_lts=None, fuse=False):
         durations = [sum(_) for _ in durations]
     tuplets = rmakers.accelerando(
         abjad.duration.durations(durations),
-        abjad.duration.durations([(1, 8), (1, 2), (1, 16)]),
-        abjad.duration.durations([(1, 2), (1, 8), (1, 16)]),
+        baca.rhythm.interpolations(
+            [(1, 8), (1, 2), (1, 16)],
+            [(1, 2), (1, 8), (1, 16)],
+        ),
         tag=tag,
     )
     voice = rmakers.wrap_in_time_signature_staff(tuplets, time_signatures)
